fc7afdce921e2d562437295e8caa993edd40d76a
rotala17 <rotala.bio@gmail.com>
2017-03-18 17:48:52 +0900

    policeを動かす
    joystickの方向に一定のスピードでpoliceが動くようにしました。進行方向に向くようになっています。境界の動作は未処理です。gangはスクリプトでやっているようですが、自分はcollidorの方がいいかなと思うので要相談です。

--- Unity_MayFes/Assets/Police_Director.cs
+++ Unity_MayFes/Assets/Police_Director.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class Police_Director : MonoBehaviour {
+    public GameObject joystick;
+    public float speed;
+    Vector2 dir;
+    Joystick j;
 	// Use this for initialization
 	void Start () 
     {
+        j = joystick.GetComponent<Joystick>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    
-	}
+        dir = j.tellDirection().normalized;
+	    if(dir.magnitude != 0)
+        {
+            transform.position += speed * (Vector3)dir * Time.deltaTime;
+        }
+        //向きを進行方向にする
+        if (dir != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            angle += 30;//defaultの向きが30度傾いているので
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
 }
