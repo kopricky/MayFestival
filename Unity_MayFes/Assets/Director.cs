eb374987489fda169da57226e044e911c8a64e0c
kopricky <kopricky@yahoo.co.jp>
2017-03-26 17:07:26 +0900

    速度バーとDirectorの変更

--- Unity_MayFes/Assets/Director.cs
+++ Unity_MayFes/Assets/Director.cs
@@ -1,23 +1,24 @@
 ﻿//大幅に修正の必要ありです
-
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Director : MonoBehaviour {
 
-    List<GameObject> gang;
+    List<GameObject> list_gang;
     float[,] dir = new float[100,100];
-    public Vector2[] speed = new Vector2[10001];
-
+    Vector2 first_force;
+    Rigidbody2D[] gang_rb = new Rigidbody2D[100];
 	// Use this for initialization
 	void Start () 
     {
-        gang = GameObject.Find("GangGenerator").GetComponent<Gang_Generator>().list_gang;
-        for (int i = 0; i < 10000; i++)
+        list_gang = GameObject.Find("GangGenerator").GetComponent<Gang_Generator>().list_gang;
+        first_force.x = 100.0f;
+        first_force.y = 100.0f;
+        for (int i = 0; i < list_gang.Count; i++)
         {
-            speed[i].x = 0.1f;
-            speed[i].y = 0.1f;
+            gang_rb[i] = list_gang[i].GetComponent<Rigidbody2D>();
+            gang_rb[i].AddForce(first_force);
         }
 	}
 	
@@ -25,72 +26,41 @@ public class Director : MonoBehaviour {
 	void Update () 
     {
         //全点対間の距離を求める。
-        for (int i = 0; i < gang.Count; i++)
+        for (int i = 0; i < list_gang.Count; i++)
         {
-            for (int j = 0; j < gang.Count; j++)
+            for (int j = 0; j < list_gang.Count; j++)
             {
-                dir[i,j] = (gang[i].transform.position - gang[j].transform.position).magnitude;
+                dir[i,j] = (list_gang[i].transform.position - list_gang[j].transform.position).magnitude;
             }
         }
         //速度に応じて色を変更する。
-        for (int i = 0; i < gang.Count; i++)
+        for (int i = 0; i < list_gang.Count; i++)
         {
-            if (speed[i].magnitude > 0.15f)
+            if (gang_rb[i].velocity.magnitude > 0.15f)
             {
-                gang[i].GetComponent<SpriteRenderer>().color = Color.magenta;
+                list_gang[i].GetComponent<SpriteRenderer>().color = Color.magenta;
             }
-            if (speed[i].magnitude < 0.05f)
+            if (gang_rb[i].velocity.magnitude < 0.05f)
             {
-                gang[i].GetComponent<SpriteRenderer>().color = Color.cyan;
+                list_gang[i].GetComponent<SpriteRenderer>().color = Color.cyan;
             }
-        }
-        //test
-        /*
-        for (int i = 0; i < gang.Count; i++)
-        {
-            for (int j = 0; j < gang.Count; j++)
-            {
-                if (gang[j].GetComponent<SpriteRenderer>().color == Color.magenta)
-                {
-                    if (dir[i, j] < 0.4f)
-                    {
-                        if (i != j && speed[i].magnitude < 0.24f)
-                        {
-                            speed[i] *= 1.2f;
-                        }
-                    }
-                }
-            }
-            for (int j = 0; j < gang.Count; j++)
-            {
-                if (gang[j].GetComponent<SpriteRenderer>().color == Color.cyan)
-                {
-                    if (dir[i, j] < 0.4f)
-                    {
-                        if (i != j && speed[i].magnitude > 0.01f)
-                        {
-                            speed[i] *= 5.0f / 6.0f;
-                        }
-                    }
-                }
-            }
-        }
-         * */
-        //各点の移動
-        for (int i = 0; i < gang.Count; i++)
-        {
-            gang[i].transform.Translate(speed[i].x, speed[i].y, 0);
         }
         //境界処理
-        for (int i = 0; i < gang.Count; i++)
+        for (int i = 0; i < list_gang.Count; i++)
         {
-            if (gang[i].transform.position.x < -7.9f || gang[i].transform.position.x > 3.9f)
+            if (list_gang[i].transform.position.x < -7.9f || list_gang[i].transform.position.x > 3.9f)
             {
-                speed[i].x *= -1;
+                Vector2 force;
+                force.x = 100.0f;
+                force.y = 0.0f;
+                gang_rb[i].AddForce(System.Math.Sign(list_gang[i].transform.position.x)*-force);
             }
-            if (gang[i].transform.position.y < -4.9f || gang[i].transform.position.y > 4.9f)
+            if (list_gang[i].transform.position.y < -4.9f || list_gang[i].transform.position.y > 4.9f)
             {
-                speed[i].y *= -1;
+                Vector2 force;
+                force.x = 0.0f;
+                force.y = 100.0f;
+                gang_rb[i].AddForce(System.Math.Sign(list_gang[i].transform.position.y)*-force);
             }
         }
     }
--- Unity_MayFes/Assets/UI_Director.cs
+++ Unity_MayFes/Assets/UI_Director.cs
@@ -1,67 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class UI_Director : MonoBehaviour {
-
-    Vector2[] speed;
-    //バーの高さの最大値
-    float height;
-    int gang_count;
-    List<GameObject> bar;
-    List<GameObject> gang;
-    Vector2 left;
-    Vector2[] bar_pos = new Vector2[30];
-    Vector2[] bar_size = new Vector2[30];
-    int[] range_count = new int[30];
-    //バーの速度に関する刻み幅
-    float range = 0.01f;
-
-	// Use this for initialization
-	void Start () 
+public class UI_Director : MonoBehaviour
+{
+    List<GameObject> list_gang, list_bar;
+    float max_speed;
+    float min_speed;
+    float speed_range;
+    float[] bar_value = new float[100];
+    Slider[] slider = new Slider[100];
+    // Use this for initialization
+    void Start()
     {
-        this.speed = GameObject.Find("Director").GetComponent<Director>().speed;
-        this.height = GameObject.Find("BarGenerator").GetComponent<Bar_Generator>().height;
-        bar = GameObject.Find("BarGenerator").GetComponent<Bar_Generator>().list_bar;
-        gang = GameObject.Find("GangGenerator").GetComponent<Gang_Generator>().list_gang;
-        for (int i = 0; i < bar.Count; i++)
+        list_gang = GameObject.Find("GangGenerator").GetComponent<Gang_Generator>().list_gang;
+        list_bar = GameObject.Find("BarGenerator").GetComponent<Bar_Generator>().list_bar;
+        for (int i = 0; i < list_bar.Count; i++)
         {
-            bar_pos[i] = bar[i].transform.position;
+            slider[i] = list_bar[i].GetComponent<Slider>();
         }
-        for (int i = 0; i < bar.Count; i++)
-        {
-            bar_size[i] = bar[i].transform.localScale;
-        }
-            this.left = GameObject.Find("BarGenerator").GetComponent<Bar_Generator>().left;
-	}
-	
-	// Update is called once per frame
-	void Update () 
+        min_speed = 0.0f;
+        max_speed = 10.0f;
+        speed_range = max_speed - min_speed;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        gang_count = 0;
-        for (int i = 0; i < bar.Count; i++)
+        for (int i = 0; i < list_bar.Count; i++)
         {
-            range_count[i] = 0;
+            slider[i].value = 0.0f;
         }
-        //速度の各刻み範囲に含まれる暴走族の数
-        for (int i = 0; i < bar.Count; i++)
+        Debug.Log(list_gang[3].GetComponent<Rigidbody2D>().mass);
+        for (int i = 0; i < list_gang.Count; i++)
         {
-            int buff = (int)(this.speed[i].magnitude / range);
-            if (buff >= bar.Count)
-            {
-                buff = bar.Count - 1;
-            }
-            range_count[buff]++;
+            float gang_speed = list_gang[i].GetComponent<Rigidbody2D>().velocity.magnitude;
+            int index = (int)(gang_speed / speed_range * list_bar.Count);
+            slider[index].value += 1.0f / list_gang.Count;
+            slider[i].value += 1.0f / list_gang.Count;
         }
-        //バーを更新
-        for (int i = 0; i < bar.Count; i++)
-        {
-            //速度の各刻み範囲に含まれる暴走族の割合
-            float rate = range_count[i] / (float)gang.Count;
-            bar_pos[i].y = left.y + rate * height / 2.0f;
-            bar[i].transform.position = bar_pos[i];
-            bar_size[i].y = rate * height;
-            bar[i].transform.localScale = bar_size[i];
-        }
-	}
+    }
 }
