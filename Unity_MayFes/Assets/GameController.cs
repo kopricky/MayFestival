54bb9dacdab494443d038658dfaadf5c66debd66
rotala17 <rotala.bio@gmail.com>
2017-04-02 14:52:45 +0900

    policeとgangの境界処理
    境界処理をpolice
    director.csとgang_property.csに書き込んだ。また境界の値はGameControllerというオブジェクトを作りそれに持たせた。

---
+++ Unity_MayFes/Assets/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public float upperBound;
+    public float lowerBound;
+    public float leftBound;
+    public float rightBound;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
